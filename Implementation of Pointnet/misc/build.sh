5423889377d4548e420ebc2c88bb4ce825ce06d0
Yide Ma <yma73@sfu.ca>
2023-03-28 17:21:09 -0700

    clean

--- Implementation of Pointnet/misc/build.sh
+++
@@ -1,6 +0,0 @@
-SCRIPT=`realpath $0`
-SCRIPTPATH=`dirname $SCRIPT`
-echo $SCRIPTPATH
-
-
-g++ -std=c++11 $SCRIPTPATH/../codes/render_balls_so.cpp -o $SCRIPTPATH/../codes/render_balls_so.so -shared -fPIC -O2 -D_GLIBCXX_USE_CXX11_ABI=0
--- cifar100-densenet-main/README.md
+++
@@ -1,3 +0,0 @@
-# cifar100-densenet
-
-This is an implementation of densenet in cifar 100. achieved around 76% accuracy which is mentioned in densenet original paper.
